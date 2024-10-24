b03b9ebfe7f951a67ad18e1d4274c3ffb2246a43
Renan Garcia <renangarciagomes@gmail.com>
2024-10-24 19:37:38 -0300

    Revert "debugging"
    This reverts commit 30b0d0831c3f952ddea462ff7457d75ac44e1da8.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,131 +1,133 @@
 import { theme } from "./theme.js";
 import { handleCustomInputRange } from "./inputRange.js";
-import { albumList, genresList } from "./productsData.js";
+import { fetchAlbums, genresList } from "./productsData.js";
 
 const renderGenreItems = (genres) => {
-    const ulGenreList = document.querySelector(".genres__list");
-  
-    genres.forEach((genre) => {
-      const liGenre = document.createElement("li");
-      liGenre.innerText = genre;
-      liGenre.classList.add("genre__item", "text3");
-      if (genre === "Todos") {
-        liGenre.classList.add("active");
-      }
-      ulGenreList.appendChild(liGenre);
-    });
-  };
+  const ulGenreList = document.querySelector(".genres__list");
 
-  const createAlbumCard = (albumData) => {
+  genres.forEach((genre) => {
+    const liGenre = document.createElement("li");
+    liGenre.innerText = genre;
+    liGenre.classList.add("genre__item", "text3");
+    if (genre === "Todos") {
+      liGenre.classList.add("active");
+    }
+    ulGenreList.appendChild(liGenre);
+  });
+};
 
-    const card = document.createElement("li");
-  
-    const albumCoverContainer = document.createElement("figure");
-    const albumCoverImg = document.createElement("img");
-    const albumDetails = document.createElement("div");
-    const albumBand = document.createElement("span");
-    const albumYear = document.createElement("span");
-    const albumGenre = document.createElement("span");
-    const albumTitle = document.createElement("h3");
-    const albumPriceContainer = document.createElement("div");
-    const albumPrice = document.createElement("h3");
-    const albumBuyButton = document.createElement("button");
-  
-  
-    card.classList.add("album__item", "slide");
-  
-  
-    albumCoverContainer.classList.add("album__cover-container");
-    albumCoverImg.classList.add("album__cover");
-    albumCoverImg.src = albumData.img;
-    albumCoverContainer.appendChild(albumCoverImg);
-  
-  
-    albumDetails.classList.add("album__details");
-  
-    albumBand.classList.add("album__band");
-    albumYear.classList.add("album__year");
-    albumGenre.classList.add("album__genre");
-  
-    albumBand.innerText = albumData.band;
-    albumYear.innerText = albumData.year;
-    albumGenre.innerText = albumData.genre;
-    albumDetails.append(albumBand, albumYear, albumGenre);
-  
-    albumTitle.classList.add("album__name");
-    albumTitle.innerText = albumData.title;
-  
-  
-    albumPriceContainer.classList.add("album__price--container");
-  
-    albumPrice.classList.add("album__price");
-    albumPrice.innerText = "R$ " + albumData.price;
-    albumBuyButton.classList.add("album__buy--button");
-    albumBuyButton.innerText = "Comprar";
-    albumPriceContainer.append(albumPrice, albumBuyButton);
-  
-    card.append(
-      albumCoverContainer,
-      albumTitle,
-      albumDetails,
-      albumPriceContainer
-    );    
-  
-    return card;
-  };
-  
-  const renderAlbumCards = (albums) => {
-    const ulAlbumList = document.querySelector(".albums__list");
-    ulAlbumList.innerHTML = "";
-  
-    albums.forEach((album) => {
-      const albumCard = createAlbumCard(album);
-      ulAlbumList.appendChild(albumCard);
-    });
-  };
-  
-  const handleFilter = (albums, genreFilter = "Todos", priceFilter) => {
-    return albums.filter(
-      (album) =>
-        (album.genre === genreFilter || genreFilter === "Todos") &&
-        album.price <= priceFilter
-    );
-  };
-  
-  const removeActiveClass = (genres) => {
-    genres.forEach((genre) => genre.classList.remove("active"));
-  };
-  
-  const handleFilterEvents = (albums) => {
-    const genres = document.querySelectorAll(".genre__item");
-    const inputPriceRange = document.querySelector(".price__input-range");
-    const spanPriceValue = document.querySelector(".price-range__value--dynamic");
-  
-    let genreCategory = "Todos";
-    let priceValue = inputPriceRange.valueAsNumber;
-  
-    genres.forEach((genre) => {
-      genre.addEventListener("click", (event) => {
-        removeActiveClass(genres);
-        genre.classList.add("active");
-        genreCategory = event.target.innerText;
-  
-        const albumsToRender = handleFilter(albums, genreCategory, priceValue);
-        renderAlbumCards(albumsToRender);
-      });
-    });
-  
-    inputPriceRange.addEventListener("input", (event) => {
-      priceValue = event.target.value;
-      spanPriceValue.innerText = "R$ " + priceValue;
+const createAlbumCard = (albumData) => {
+  const card = document.createElement("li");
+
+  const albumCoverContainer = document.createElement("figure");
+  const albumCoverImg = document.createElement("img");
+  const albumDetails = document.createElement("div");
+  const albumBand = document.createElement("span");
+  const albumYear = document.createElement("span");
+  const albumGenre = document.createElement("span");
+  const albumTitle = document.createElement("h3");
+  const albumPriceContainer = document.createElement("div");
+  const albumPrice = document.createElement("h3");
+  const albumBuyButton = document.createElement("button");
+
+  card.classList.add("album__item", "slide");
+
+  albumCoverContainer.classList.add("album__cover-container");
+  albumCoverImg.classList.add("album__cover");
+  albumCoverImg.src = albumData.img;
+  albumCoverContainer.appendChild(albumCoverImg);
+
+  albumDetails.classList.add("album__details");
+
+  albumBand.classList.add("album__band");
+  albumYear.classList.add("album__year");
+  albumGenre.classList.add("album__genre");
+
+  albumBand.innerText = albumData.band;
+  albumYear.innerText = albumData.year;
+  albumGenre.innerText = albumData.genre;
+  albumDetails.append(albumBand, albumYear, albumGenre);
+
+  albumTitle.classList.add("album__name");
+  albumTitle.innerText = albumData.title;
+
+  albumPriceContainer.classList.add("album__price--container");
+
+  albumPrice.classList.add("album__price");
+  albumPrice.innerText = "R$ " + albumData.price;
+  albumBuyButton.classList.add("album__buy--button");
+  albumBuyButton.innerText = "Comprar";
+  albumPriceContainer.append(albumPrice, albumBuyButton);
+
+  card.append(
+    albumCoverContainer,
+    albumTitle,
+    albumDetails,
+    albumPriceContainer
+  );
+
+  return card;
+};
+
+const renderAlbumCards = (albums) => {
+  const ulAlbumList = document.querySelector(".albums__list");
+  ulAlbumList.innerHTML = "";
+
+  albums.forEach((album) => {
+    const albumCard = createAlbumCard(album);
+    ulAlbumList.appendChild(albumCard);
+  });
+};
+
+const handleFilter = (albums, genreFilter = "Todos", priceFilter) => {
+  return albums.filter(
+    (album) =>
+      (album.genre === genreFilter || genreFilter === "Todos") &&
+      album.price <= priceFilter
+  );
+};
+
+const removeActiveClass = (genres) => {
+  genres.forEach((genre) => genre.classList.remove("active"));
+};
+
+const handleFilterEvents = (albums) => {
+  const genres = document.querySelectorAll(".genre__item");
+  const inputPriceRange = document.querySelector(".price__input-range");
+  const spanPriceValue = document.querySelector(".price-range__value--dynamic");
+
+  let genreCategory = "Todos";
+  let priceValue = inputPriceRange.valueAsNumber;
+
+  genres.forEach((genre) => {
+    genre.addEventListener("click", (event) => {
+      removeActiveClass(genres);
+      genre.classList.add("active");
+      genreCategory = event.target.innerText;
+
       const albumsToRender = handleFilter(albums, genreCategory, priceValue);
-  
       renderAlbumCards(albumsToRender);
     });
-  };
-  
+  });
+
+  inputPriceRange.addEventListener("input", (event) => {
+    priceValue = event.target.value;
+    spanPriceValue.innerText = "R$ " + priceValue;
+    const albumsToRender = handleFilter(albums, genreCategory, priceValue);
+
+    renderAlbumCards(albumsToRender);
+  });
+};
+
+const initializeApp = async () => {
   renderGenreItems(genresList);
-  handleFilterEvents(albumList);
-  renderAlbumCards(albumList);
+
+  const albums = await fetchAlbums();
+  handleFilterEvents(albums);
+  renderAlbumCards(albums);
+
   theme();
   handleCustomInputRange();
+};
+
+initializeApp();
