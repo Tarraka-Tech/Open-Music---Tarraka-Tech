abb94e1627acd8f54d56549e1c37c94f9135120d
Renan Garcia <renangarciagomes@gmail.com>
2024-10-24 19:28:05 -0300

    Update theme.js

--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -2,11 +2,10 @@ const botao = document.querySelector(".header__btn");
 const html = document.querySelector("html");
 
 const storedTheme = localStorage.getItem("@openMusic:");
-
 let darkMode = storedTheme ? JSON.parse(storedTheme) : false;
 
 export const theme = () => {
-  darkMode = !darkMode;
+  darkMode = !darkMode; 
   html.classList.toggle("dark-mode", darkMode);
   botao.classList.toggle("header__btn--dark-mode", darkMode);
   localStorage.setItem("@openMusic:", JSON.stringify(darkMode));
