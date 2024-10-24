266a0737b12fec4c5c14fbf30d03ad8c4e2c6537
Renan Garcia <renangarciagomes@gmail.com>
2024-10-24 19:32:12 -0300

    debuggin

--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -1,23 +1,22 @@
 const botao = document.querySelector(".header__btn");
+let darkMode = false;
 const html = document.querySelector("html");
 
-const storedTheme = localStorage.getItem("@openMusic:");
-let darkMode = storedTheme ? JSON.parse(storedTheme) : false;
-
 export const theme = () => {
-  darkMode = !darkMode; 
-  html.classList.toggle("dark-mode", darkMode);
-  botao.classList.toggle("header__btn--dark-mode", darkMode);
+  darkMode = !darkMode;
+  html.classList.toggle("dark-mode");
+  botao.classList.toggle("header__btn--dark-mode");
   localStorage.setItem("@openMusic:", JSON.stringify(darkMode));
-};
 
-if (darkMode) {
-  html.classList.add("dark-mode");
-  botao.classList.add("header__btn--dark-mode");
-} else {
-  html.classList.remove("dark-mode");
-  botao.classList.remove("header__btn--dark-mode");
-}
+  const storedTheme = localStorage.getItem("@openMusic:");
+  if (storedTheme) {
+    darkMode = JSON.parse(storedTheme);
+  }
+  if (darkMode) {
+    html.classList.add("dark-mode");
+    botao.classList.add("header__btn--dark-mode");
+  }
+};
 
 botao.addEventListener("click", (event) => {
   event.preventDefault();
